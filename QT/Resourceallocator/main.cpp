3670e4a49cfa57875479d8d252b1e8a3221828a3
Koto-Sakura <140402680+Koto-Sakura@users.noreply.github.com>
2024-11-21 23:12:47 +0800

    Delete QT/Resourceallocator directory

--- QT/Resourceallocator/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QApplication>
-#include "resourceallocator.h"
-
-int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
-    ResourceAllocator window;
-    window.show();
-    return app.exec();
-}
